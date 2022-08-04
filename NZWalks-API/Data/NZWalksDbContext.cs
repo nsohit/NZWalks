ae3f768c59cda07f6af603f614d775f279ecca68
nsohit <nursohit@gmail.com>
2022-08-04 15:07:06 +0700

    part2

---
+++ NZWalks-API/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks_API.Models.Domain;
+
+namespace NZWalks_API.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Region>? Regions { get; set; }
+        public DbSet<Walk>? Walks { get; set; }
+        public DbSet<WalkDificulty>? WalkDificulty { get; set; }
+    }
+}
